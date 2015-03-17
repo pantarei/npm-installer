a20c51587214ca6e82dab727b770011e0ae4cf31
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-03-17 12:25:26 +0800

    Update with initial install script.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o xtrace
+
+# Define variables.
+BRANCH="master"
+TMP_DIR=`mktemp -d -t npm-installer.XXXXXX`
+
+# Install packages globally.
+npm -g install bower cordova less
+npm -g update
